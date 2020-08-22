b62e129b1e92ac5be706dc376784472f938b055d
ramratan21 <ramrinva12@gmail.com>
2020-08-22 22:59:19 +0530

    Updates

---
+++ precache-manifest.6512b2ee8bc1620ae48e96283812c1d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8619a359ba73a6923fb97f26603085",
+    "url": "/Currency_converter/index.html"
+  },
+  {
+    "revision": "78aedafd34acdc6326a9",
+    "url": "/Currency_converter/static/css/main.e6970bfd.chunk.css"
+  },
+  {
+    "revision": "bc13682f40c606b91e1e",
+    "url": "/Currency_converter/static/js/2.deb60666.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency_converter/static/js/2.deb60666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78aedafd34acdc6326a9",
+    "url": "/Currency_converter/static/js/main.a1f5171c.chunk.js"
+  },
+  {
+    "revision": "e614409dd53e51f91670",
+    "url": "/Currency_converter/static/js/runtime-main.b8325aa0.js"
+  }
+]);
